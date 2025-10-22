6652fb4ff3e98fb942473a70816331ef24dbc5da
David Fernando Bedoya Ramirez <davidfernando240307@gmail.com>
2025-10-22 10:47:27 -0500

    Add Backend and Frontend projects

---
+++ Frontend/src/models/User.ts
@@ -0,0 +1,17 @@
+import { Session } from "./Session";
+export interface User {
+    id?: number;
+    name?: string;
+    email?: string;
+    password?:string;
+    age?: number;
+    city?: string;
+    phone?: string;
+    is_active?: boolean;
+    token?:string;
+    sessions?: Session[];
+}
+
+//aqui se pueden escribir metodos, aqui se tiran las cardinalidades de las relaciones
+//ejemplo si un usuario tiene muchos posts, se puede hacer un metodo que traiga los posts de ese usuario
+//pero en este caso no hay relaciones, asi que no hay metodos
